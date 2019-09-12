be627ea731c4912b31552d6a47b1697274dc2fca
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2019-09-12 05:16:27 +0200

    fix bug in type of column

--- inst/scripts/Exp1_R25_pept.R
+++ inst/scripts/Exp1_R25_pept.R
@@ -4,7 +4,7 @@ library("DAPAR")
 exprsFile <- system.file("extdata", "Exp1_R25_pept.txt", package="DAPARdata")
 
 metadataFile <- system.file("extdata", "samples_Exp1_R25.txt", package="DAPARdata")
-metadata <- read.table(metadataFile, header=TRUE, sep="\t", as.is=TRUE)
+metadata <- read.table(metadataFile, header=TRUE, sep="\t", as.is=TRUE, stringsAsFactors = FALSE)
 indExpData <- c(56:61)
 indFData <- c(1:55,62:71)
 indiceID <- 64
