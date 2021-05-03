c6e3c1dcacb59b2d95eab6861b6e265872bb1a19
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2021-05-03 07:53:12 +0200

    bug fixed for duplicate names

--- inst/scripts/rebuildDatasets.R
+++ inst/scripts/rebuildDatasets.R
@@ -1,39 +1,39 @@
 source(file = 'Exp1_R2_pept.R')
 save(Exp1_R2_pept, file = '../../data/Exp1_R2_pept.RData')
-saveRDS(Exp1_R2_pept, '../extdata/Exp1_R2_pept.MSnSet')
+saveRDS(Exp1_R2_pept, '../extdata/Exp1_R2_pept.MSnset')
 
 source(file = 'Exp1_R2_prot.R')
 save(Exp1_R2_prot, file = '../../data/Exp1_R2_prot.RData')
-saveRDS(Exp1_R2_prot, '../extdata/Exp1_R2_prot.MSnSet')
+saveRDS(Exp1_R2_prot, '../extdata/Exp1_R2_prot.MSnset')
 
 source(file = 'Exp1_R25_pept.R')
 save(Exp1_R25_pept, file = '../../data/Exp1_R25_pept.RData')
-saveRDS(Exp1_R25_pept, '../extdata/Exp1_R25_pept.MSnSet')
+saveRDS(Exp1_R25_pept, '../extdata/Exp1_R25_pept.MSnset')
 
 source(file = 'Exp1_R25_prot.R')
 save(Exp1_R25_prot, file = '../../data/Exp1_R25_prot.RData')
-saveRDS(Exp1_R25_prot, '../extdata/Exp1_R25_prot.MSnSet')
+saveRDS(Exp1_R25_prot, '../extdata/Exp1_R25_prot.MSnset')
 
 source(file = 'Exp2_R2_pept.R')
 save(Exp2_R2_pept, file = '../../data/Exp2_R2_pept.RData')
-saveRDS(Exp2_R2_pept, '../extdata/Exp2_R2_pept.MSnSet')
+saveRDS(Exp2_R2_pept, '../extdata/Exp2_R2_pept.MSnset')
 
 source(file = 'Exp2_R2_prot.R')
 save(Exp2_R2_prot, file = '../../data/Exp2_R2_prot.RData')
-saveRDS(Exp2_R2_prot, '../extdata/Exp2_R2_prot.MSnSet')
+saveRDS(Exp2_R2_prot, '../extdata/Exp2_R2_prot.MSnset')
 
 source(file = 'Exp2_R10_pept.R')
 save(Exp2_R10_pept, file = '../../data/Exp2_R10_pept.RData')
-saveRDS(Exp2_R10_pept, '../extdata/Exp2_R10_pept.MSnSet')
+saveRDS(Exp2_R10_pept, '../extdata/Exp2_R10_pept.MSnset')
 
 source(file = 'Exp2_R10_prot.R')
 save(Exp2_R10_prot, file = '../../data/Exp2_R10_prot.RData')
-saveRDS(Exp2_R10_prot, '../extdata/Exp2_R10_prot.MSnSet')
+saveRDS(Exp2_R10_prot, '../extdata/Exp2_R10_prot.MSnset')
 
 source(file = 'Exp2_R100_pept.R')
 save(Exp2_R100_pept, file = '../../data/Exp2_R100_pept.RData')
-saveRDS(Exp2_R100_pept, '../extdata/Exp2_R100_pept.MSnSet')
+saveRDS(Exp2_R100_pept, '../extdata/Exp2_R100_pept.MSnset')
 
 source(file = 'Exp2_R100_prot.R')
 save(Exp2_R100_prot, file = '../../data/Exp2_R100_prot.RData')
-saveRDS(Exp2_R100_prot, '../extdata/Exp2_R100_prot.MSnSet')
+saveRDS(Exp2_R100_prot, '../extdata/Exp2_R100_prot.MSnset')
