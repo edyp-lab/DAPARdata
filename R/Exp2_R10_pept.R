7470dd192bf6d2865214e7bc7ac9c1398b115304
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2022-06-17 14:03:20 +0200

    clean package to pass BiocCheck()

---
+++ R/Exp2_R10_pept.R
@@ -0,0 +1,48 @@
+#' @title Builds Exp2_R10_pept dataset
+#' 
+#' @rdname Exp2_R10_pept
+#' @examples 
+#' builds_Exp2_R10_pept()
+#' 
+#' @importFrom utils read.table
+#' 
+builds_Exp2_R10_pept <- function(){
+  
+  if (! requireNamespace("DAPAR", quietly = TRUE)) {
+    stop("Please install DAPAR: BiocManager::install('DAPAR')")
+  }
+  
+  exprsFile <- system.file(
+    "extdata", 
+    "Exp2_R10_pept.txt", 
+    package="DAPARdata"
+    )
+
+metadataFile <- system.file(
+  "extdata", "samples_Exp2_R10.txt", 
+  package="DAPARdata")
+
+metadata <- read.table(
+  metadataFile, 
+  header = TRUE, 
+  sep = "\t", 
+  as.is = TRUE
+  )
+
+Exp2_R10_pept <- DAPAR::createMSnset(
+  file = exprsFile, 
+  metadata = metadata, 
+  indExpData = seq_len(6), 
+  colnameForID = 'id', 
+  indexForMetacell = seq.int(from = 49, to = 54),
+  logData = TRUE, 
+  replaceZeros = TRUE,
+  pep_prot_data = "peptide",
+  proteinId = "Protein_group_IDs",
+  software = 'maxquant'
+  )
+
+Exp2_R10_pept <- Set_X_CC_infos(Exp2_R10_pept)
+
+Exp2_R10_pept
+}
